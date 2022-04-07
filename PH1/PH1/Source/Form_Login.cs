18a4558c57047e3a73aa099cf1c852b284529c69
Dam Hong Duc <hongduc001h@gmail.com>
2022-04-07 23:19:26 +0700

    add login + main

---
+++ PH1/PH1/Source/Form_Login.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PH1
+{
+    public partial class Form_Login : Form
+    {
+        public Form_Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_login_Click(object sender, EventArgs e)
+        {
+            Form_Main form_main = new Form_Main();
+            form_main.ShowDialog();
+            this.Close();
+
+        }
+
+    }
+}
---
+++ PH1/PH1/Source/Form_Main.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PH1
+{
+    public partial class Form_Main : Form
+    {
+        public Form_Main()
+        {
+            InitializeComponent();
+        }
+
+        // mở 1 form con
+        private Form activeform = null;
+        private void openChildForm(Form childForm)
+        {
+            if (activeform != null)
+                activeform.Close();
+            activeform = childForm;
+            childForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            childForm.Dock = DockStyle.Fill;
+            panelChildForm_KH.Controls.Add(childForm);
+            panelChildForm_KH.Tag = childForm;
+            childForm.BringToFront();
+            childForm.Show();
+        }
+
+        // xử lí chuyển màu khi click vào button
+        private Button currentButton;
+        private void ActivateButton(object btnSender)
+        {
+            if (btnSender != null)
+            {
+                if (currentButton != (Button)btnSender)
+                {
+                    DisableButton();
+                    Color color = ColorTranslator.FromHtml("#4169E1");
+                    currentButton = (Button)btnSender;
+                    currentButton.BackColor = color;
+                    currentButton.ForeColor = Color.White;
+                }
+            }
+        }
+        private void DisableButton()
+        {
+            foreach (Control previousBtn in panelMenu.Controls)
+            {
+                if (previousBtn.GetType() == typeof(Button))
+                {
+                    previousBtn.BackColor = Color.FromArgb(39, 39, 58);
+                    previousBtn.ForeColor = Color.Gainsboro;
+                }
+            }
+        }
+
+        private void Set_Center_Username()
+        {
+            int y = (panel_username.Height / 2) - (label_username.Height / 2);
+            int x = (panel_username.Width / 2) - (label_username.Width / 2);
+            label_username.Location = new Point(x, y+5);
+            label_welcome.Location = new Point(x,y-40);
+        }
+
+        private void Form_Main_Load(object sender, EventArgs e)
+        {
+            Set_Center_Username();
+        }
+    }
+}
